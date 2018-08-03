be5abbd7ca5bbe9f9ffbb19999ef3fa846a6cade
ArmandDu <dupuis.armand42@gmail.com>
2018-08-03 21:59:00 +0300

    assignment 1

---
+++ color-game/src/components/Squares.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export class Squares extends React.Component {
+	render() {
+		return (
+			<div className="squares">
+				<div className="square" />
+				<div className="square" />
+				<div className="square" />
+				<div className="square" />
+				<div className="square" />
+				<div className="square" />
+			</div>
+		);
+	}
+}
+
+export default Squares;
