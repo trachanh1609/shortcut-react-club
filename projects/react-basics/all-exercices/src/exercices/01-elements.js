8bd2c110b8f724f00d26e30b2dcfdf44abad6fbd
carole <arbogast.carole@gmail.com>
2018-10-01 11:16:19 +0300

    spellcheck

--- projects/react-basics/all-exercices/src/exercices/01-elements.js
+++ projects/react-basics/all-exercices/src/exercices/01-elements.js
@@ -3,11 +3,11 @@ import React from "react"
  * 01-1 - A first JSX element
  *
  * 
- * JSX is javascript XML. it let you write HTML-like markup in Javascript.
- * Let's give it a test
+ * JSX is javascript XML. It lets you write HTML-like markup in Javascript.
+ * Let's give it a test.
  * 
- * export a variable named "helloWorld" and assign it a JSX element.
- * The JSX element must have the text "Hello World!" wrapped in a h1 element
+ * Export a variable named "helloWorld" and assign it a JSX element.
+ * The JSX element must have the text "Hello World!" wrapped in an h1 element.
  * 
  * NB: we are not creating components but assiging HTML-like markup into a Javascript variable.
  *  
@@ -20,16 +20,16 @@ import React from "react"
  *  
  * Now let's make a more complex element.
  * 
- * export a variable named "nestedJSX" and assign it a JSX element.
+ * Export a variable named "nestedJSX" and assign it a JSX element.
  * The outer element should be a div with an attribute `className` of "card".
  * Inside the div, insert a h4 with content "John Doe" and a p element with content "Age: 42" 
  * 
  * NB: convention for complex JSX.
- * 1. To improve readibility, wrap your element in parenthesis ()
+ * 1. To improve readability, wrap your element in parentheses ()
  * 2. One JSX element per line
  * 3. Use indentation
  * 
- * exemple:
+ * Example:
  * //bad
  * const element = <div><input/><button>Click me</button></div>
  * 
@@ -49,13 +49,13 @@ import React from "react"
 /**
  * 01-3 - interpolating data part 1
  * 
- * So far we only created JSX only content. You could have just created a html file.
+ * So far, we've created JSX-only content. You could have just created an html file.
  * Now we are going to mix Javascript inside it.
  * 
  * Below is a Javascript variable. You are going to insert that variable inside your JSX.
- * To do so, you have to wrap your JS experession inside curly braces {}. 
+ * To do so, you have to wrap your JS expression inside curly braces {}. 
  * 
- * export a variable named "interpolatingPart1" that has a div element.
+ * Export a variable named "interpolatingPart1" that has a div element.
  * Inside that div should be the content of the variable 'interpolateTest"
  *  
  */
@@ -70,9 +70,9 @@ export const interpolateText = "I am a JS string"
  * String and Numbers are easy because they are primitives.
  * Let's create a more complex element by trying to interpolate an object.
  * 
- * Below is a Javascript Object. You are going to print it's attributes inside a card.
+ * Below is a Javascript Object. You are going to print its attributes inside a card.
  * 
- * export a variable "interpolatingPart2". The content should be the same as the exercice 2 with execptions:
+ * Export a variable "interpolatingPart2". The content should be the same as the exercice 2, with execptions:
  * - the h4 should print the correct user name
  * - the p should print the correct age
  */
@@ -89,14 +89,14 @@ export const interpolateUser = {name: "Jeanne", age: 30};
  * 
  * Below is an array of Object. The goal is to render an unordered list (ul) with interpolating the content of the array below.
  * 
- * export a variable "interpolatingPart3". It should contain an ul with the correct number of li elements.
+ * Export a variable "interpolatingPart3". It should contain an ul with the correct number of li elements.
  * In order to do so, you have to use the Array.protoptype.map() method in between your ul tags.
  * 
- * The Array.prototype.map() takes a callback function. The callback function will be called with each element in the array a parameter.
+ * The Array.prototype.map() takes a callback function. The callback function will be called with each element in the array as parameter.
  * For each of the item in the array you must return some JSX element.
  * 
- * the returned JSX should be a li with two children:
- * - a h4 element with text `Flavor: ${the flavor}`
+ * The returned JSX should be a li with two children:
+ * - an h4 element with text `Flavor: ${the flavor}`
  * - a p element with text: `Price: ${the price}€`
  * 
  */
@@ -123,12 +123,12 @@ export const interpolateArray = [{flavor: "chocolate", cost: 2}, {flavor: "vanil
  /**
  * 01-7 - interpolating dynamic data part 1
  *  
- * Now we are going to make some slightly more interesting elements: We are giving ourselve the possibility to reuse the elements!
+ * Now we are going to make some slightly more interesting elements: we are giving ourselves the possibility to reuse the elements!
  * 
- * export a function named "elementFactory". This function accepts two parameters:
+ * Export a function named "elementFactory". This function accepts two parameters:
  * - name: String
  * - age: Number
- * It should return the same JSX element as exercice 2 and 4 but uses the parameter of the function as data source
+ * It should return the same JSX element as exercice 2 and 4 but uses the parameter of the function as data source.
  */
 
 
@@ -137,10 +137,10 @@ export const interpolateArray = [{flavor: "chocolate", cost: 2}, {flavor: "vanil
   /**
  * 01-8 - interpolating dynamic data part 2
  *  
- * The previous example works fine but what if you want to have dozen of parameters? You would have to refactor your code every time.
+ * The previous example works fine, but what if you want to have dozens of parameters? You would have to refactor your code every time.
  * So let's have a slightly more generic approach.
  * 
- * export a function named "UserCardComponent" that accepts a parameter named "props". This parameter will be an object.
+ * Export a function named "UserCardComponent" that accepts a parameter named "props". This parameter will be an object.
  * 
  * You will expect the "props" parameter to have a property "name" of type String and "age" of type Number.
  * 
